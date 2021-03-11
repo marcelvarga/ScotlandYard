c018d7f8e94d085a66922906622ab5b687453c28
Marcel <marcelvarga201@gmail.com>
2021-03-11 00:24:57 +0000

    Started working on MyGameStateFactory.java Implementing Constructor, currently passing 5 tests Left off the guide at the Initialization part

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -1,27 +1,57 @@
 package uk.ac.bris.cs.scotlandyard.model;
 
 import com.google.common.collect.ImmutableList;
-
+import com.google.common.collect.ImmutableSet;
+import java.util.*;
 import javax.annotation.Nonnull;
-
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
+import uk.ac.bris.cs.scotlandyard.model.Piece.*;
+
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
 
 /**
  * cw-model
  * Stage 1: Complete this class
- * asfobowevbewvin0b9veq
  */
 public final class MyGameStateFactory implements Factory<GameState> {
 
+	private final class MyGameState implements GameState {
+
+		private MyGameState(
+				final GameSetup setup,
+				final ImmutableSet<Piece> remaining,
+				final ImmutableList<LogEntry> log,
+				final Player mrX,
+				final List<Player> detectives){
+			this.setup = setup;
+			this.remaining = remaining;
+			this.log = log;
+			this.mrX = mrX;
+			this.detectives = detectives;
+		}
+		private GameSetup setup;
+		private ImmutableSet<Piece> remaining;
+		private ImmutableList<LogEntry> log;
+		private Player mrX;
+		private List<Player> detectives;
+		private ImmutableList<Player> everyone;
+		private ImmutableSet<Move> moves;
+		private ImmutableSet<Piece> winner;
+
+		@Override public GameSetup getSetup() {  return setup; }
+		@Override public ImmutableSet<Piece> getPlayers() { return null; }
+		@Override public Optional<Integer> getDetectiveLocation(Detective detective) { return null; }
+		@Override public Optional<TicketBoard> getPlayerTickets(Piece piece) { return null; }
+		@Override public ImmutableList<LogEntry> getMrXTravelLog() { return null; }
+		@Override public ImmutableSet<Piece> getWinner() { return winner; }
+ 		@Override public ImmutableSet<Move> getAvailableMoves() { return moves; }
+ 		@Override public GameState advance(Move move) {  return null;  }
+	}
 	@Nonnull @Override public GameState build(
 			GameSetup setup,
 			Player mrX,
 			ImmutableList<Player> detectives) {
-		// TODO
-		// Test here
-		throw new RuntimeException("Implement me!");
 
+		return new MyGameState(setup, ImmutableSet.of(MrX.MRX), ImmutableList.of(), mrX, detectives);
 	}
-
 }
