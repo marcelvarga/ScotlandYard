b43b4b430016eea55eb90e445dd0ce24775e3ae0
OliverG42 <ji20923@bristol.ac.uk>
2021-03-22 16:36:13 +0000

    Implemented chooseMove
    70 tests pass

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
@@ -5,6 +5,9 @@ import com.google.common.collect.ImmutableList;
 import javax.annotation.Nonnull;
 
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+import uk.ac.bris.cs.scotlandyard.model.Model.Observer;
+import uk.ac.bris.cs.scotlandyard.model.Model.Observer.Event;
+import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
 
 /**
  * cw-model
@@ -13,16 +16,35 @@ import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
 public final class MyModelFactory implements Factory<Model> {
 
 	public MyModelFactory() {
+		//Assumed parameters
 		final Board.GameState game;
-		final ImmutableList<Model.Observer> observers;
+		final ImmutableList<Observer> observers;
 	}
+	Board.GameState game;
+	ImmutableList<Observer> observers;
 
+	public void chooseMove(@Nonnull Move move){
+		//MOVE!
+		game.advance(move);
+
+		//Figure out whether the game has ended
+		Event e;
+		if (game.getWinner().isEmpty()) e = Event.GAME_OVER;
+		else e = Event.MOVE_MADE;
+
+		//Notify each observer the event
+		for(Observer observer : observers) {
+			observer.onModelChanged(game, e);
+		}
+	}
 
 	@Nonnull @Override public Model build(GameSetup setup,
 										  Player mrX,
 										  ImmutableList<Player> detectives) {
 
-		//IMPLEMENT ME!
-		return null;
+		//Presumably build a gameState
+		//This creates recursive loop, so ALL OBSERVER TESTS FAIL
+		Model g = build(setup, mrX, detectives);
+		return g;
 	}
 }
